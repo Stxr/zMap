89d97c27c88176848596fd0aedfb8ae630dff741
Stxr <stxrun@qq.com>
2017-09-08 21:51:15 +0800

    实现了定位和标点的功能

--- app/src/main/java/com/stxrun/zmap/MainActivity.java
+++ app/src/main/java/com/stxrun/zmap/MainActivity.java
@@ -2,16 +2,24 @@ package com.stxrun.zmap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
 import com.amap.api.maps.model.MyLocationStyle;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AMap.OnMapClickListener{
     MapView mMapView = null;
     AMap aMap=null;
     MyLocationStyle myLocationStyle;
+    Marker marker;
+    private UiSettings mUiSettings;//定义一个UiSettings对象
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,19 +31,28 @@ public class MainActivity extends AppCompatActivity {
         if (aMap == null) {
             aMap = mMapView.getMap();
         }
-
+        Log.e("located", "ok");
+        aMap.setOnMapClickListener(this);
         myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_MAP_ROTATE_NO_CENTER) ;//定位一次，且将视角移动到地图中心点。
         myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
-        aMap.showIndoorMap(true);     //true：显示室内地图；false：不显示；
+        myLocationStyle.showMyLocation(true);
         aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+        aMap.showIndoorMap(true);     //true：显示室内地图；false：不显示；
         aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
         aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        mUiSettings = aMap.getUiSettings();//实例化UiSettings类对象
+        mUiSettings.setScaleControlsEnabled(true);//控制比例尺控件是否显示
+
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
         //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
         mMapView.onDestroy();
+//        if(null != mlocationClient){
+//            mlocationClient.onDestroy();
+//        }
     }
     @Override
     protected void onResume() {
@@ -54,5 +71,15 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
         mMapView.onSaveInstanceState(outState);
+    }
+
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        Log.e("located", latLng.toString());
+        if (marker != null) {
+            marker.destroy();
+        }
+        marker = aMap.addMarker(new MarkerOptions().position(latLng).title("坐标").snippet(latLng.toString()));
     }
 }
